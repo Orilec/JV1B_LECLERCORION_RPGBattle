f8ccc194b7b349caac5a4e280383a88e60c1d0a2
Orion Leclerc <orion.leclerc@gmail.com>
2021-11-26 11:59:39 +0100

    1

--- script.js
+++ script.js
@@ -5,8 +5,35 @@ hero1= document.getElementById("hero1")
 dialogue= document.getElementById("dialogue")
 nom1= document.getElementById("nom_joueur1")
 pvjoueur1= document.getElementById("pv_joueur1")
+hero_actif= hero1
+monstre_actif= monstre1
 attaquejoueur1= 5;
 attaquemonstre1= 5;
+
+function riposte(){
+        dialogue.innerHTML= "Monstre1 riposte et inflige "+ attaquemonstre1 + " dégats à " + nom1.innerHTML;
+      
+      monstre1.animate([
+        { transform: 'translateX(0px)' },
+        { transform: 'translateX(-50px)' },
+        { transform: 'translateX(0px)' },
+  
+      ], {
+        duration: 500,
+        iterations: 1
+      });
+      setTimeout(function(){     
+        hero1.animate([
+        { opacity: '100%' },
+        { opacity: '0%' },
+        { opacity: '100%' },
+      ], {
+        duration: 500,
+        iterations: 1
+      }); }, 500);
+      pvjoueur1.innerHTML= pvjoueur1.innerHTML- attaquemonstre1;
+      dialogue.innerHTML= "Monstre1 riposte et inflige "+ attaquemonstre1 + " dégats à " + nom1.innerHTML;
+}
 
 attaquebutton.onclick= function(){
     dialogue.innerHTML= nom1.innerHTML + " attaque et inflige "+ attaquejoueur1 + " dégats à monstre1";
@@ -31,33 +58,11 @@ attaquebutton.onclick= function(){
     });
     pvmonstre1.innerHTML= pvmonstre1.innerHTML-attaquejoueur1;
   }, 500);
-    
-    
-    setTimeout(function(){
-      
-      dialogue.innerHTML= "Monstre1 riposte et inflige "+ attaquemonstre1 + " dégats à " + nom1.innerHTML;
-      
-      monstre1.animate([
-        { transform: 'translateX(0px)' },
-        { transform: 'translateX(-50px)' },
-        { transform: 'translateX(0px)' },
   
-      ], {
-        duration: 500,
-        iterations: 1
-      });
-      setTimeout(function(){     
-        hero1.animate([
-        { opacity: '100%' },
-        { opacity: '0%' },
-        { opacity: '100%' },
-      ], {
-        duration: 500,
-        iterations: 1
-      }); }, 500);
-      pvjoueur1.innerHTML= pvjoueur1.innerHTML- attaquemonstre1;
+  setTimeout(function(){
+      riposte()
     }, 2000);
-    
-   
+}    
 
-}
+
+
